505644aad05c3c5568eabb2a29ed85d777166d41
rallen0150 <rallen0150@gmail.com>
2016-11-01 20:46:45 -0400

    historic isn't working yet

--- nfl_app/views.py
+++ nfl_app/views.py
@@ -10,26 +10,24 @@ class IndexView(TemplateView):
     def get_context_data(self):
         context = super().get_context_data()
         if self.request.GET:
-            url = 'http://www.nfl.com/players/search?category=name&filter={}&playerType=current'
-            data = requests.get(url.format(self.request.GET.get('player_name')))
+            url = 'http://www.nfl.com/players/search?category=name&filter={}&playerType={}'
+            data = requests.get(url.format(self.request.GET.get('player_name'), self.request.GET.get('playerType')))
             # print(data.text)
             souper = BeautifulSoup(data.text, 'html.parser')
-            table = souper.find('table', {'class': 'data-table1'})
-            all_a_tags = table.findAll('a')[::2]#[43].get('href')
-            # for x in all_a_tags:
-            #     print(x.get('href'))
-            player_link = []
-            player_link=[(x.get('href'), x.get_text()) for x in all_a_tags]
-                # player_link.append(x.get('href'))
-                # player_link.append(x.get_text())
-
-            print(player_link)
-            context['player_name'] = player_link
-            # for counter, tag in enumerate(all_a_tags):
-            #     print(counter, tag)
-            # print(data.text)
-            # print(counter, player_link)
-        return context
+            if self.request.GET.get('playerType') == 'current':
+                table = souper.find('table', {'class': 'data-table1'})
+                all_a_tags = table.findAll('a')[::2]
+                player_link = []
+                player_link=[(x.get('href'), x.get_text()) for x in all_a_tags]
+                # print(player_link)
+                context['player_name'] = player_link
+                return context
+            elif self.request.GET.get('playerType') == 'historical':
+                table = souper.find('table', {'class': 'data-table1'})
+                all_a_tags = table.find_all('a')
+                player_link = [(x.get('href'), x.get_text()) for x in all_a_tags]
+                context['url_links'] = player_link
+                return context
 
 class PlayerView(TemplateView):
     template_name = 'player.html'
@@ -38,9 +36,9 @@ class PlayerView(TemplateView):
         context = super().get_context_data()
         page = requests.get('http://www.nfl.com/' + player_url)
         souper = BeautifulSoup(page.text, 'html.parser')
-        context['table'] = souper.find_all('table', {'class': 'data-table1'})[1].contents
-        # for x in context['table']:
-        #     x.replace('\n', '')
-        print(context['table'])
-
-        return context
+        if self.request.GET.get('playerType') == 'current':
+            context['table'] = souper.find_all('table', {'class': 'data-table1'})[1].contents
+            print(context['table'])
+        else:
+            context['table'] = souper.findAll('table', {'class': 'data-table1'})
+            return context
